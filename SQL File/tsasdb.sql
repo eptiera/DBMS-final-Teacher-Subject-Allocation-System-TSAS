78ca4826aca91782b1b6b904f853983930390e75
Epti Ara Era <22203177@iubat.edu>
2024-04-30 01:43:39 +0600

    testing

---
+++ SQL File/tsasdb.sql
@@ -0,0 +1,213 @@
+CREATE DATABASE tsasdb;
+USE tsasdb;
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 19, 2024 at 03:56 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tsasdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbladmin`
+--
+
+CREATE TABLE `tbladmin` (
+  `ID` int(10) NOT NULL,
+  `AdminName` varchar(200) DEFAULT NULL,
+  `UserName` varchar(200) DEFAULT NULL,
+  `MobileNumber` bigint(10) DEFAULT NULL,
+  `Email` varchar(200) DEFAULT NULL,
+  `Password` varchar(200) DEFAULT NULL,
+  `AdminRegdate` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbladmin`
+--
+
+INSERT INTO `tbladmin` (`ID`, `AdminName`, `UserName`, `MobileNumber`, `Email`, `Password`, `AdminRegdate`) VALUES
+(1, 'Epti Ara Era', 'admin', 5689784592, 'admin@gmail.com', 'f925916e2754e5e03f75dd58a5733251', '2024-04-19 11:58:35');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblcourse`
+--
+
+CREATE TABLE `tblcourse` (
+  `ID` int(10) NOT NULL,
+  `BranchName` varchar(200) DEFAULT NULL,
+  `CourseName` varchar(200) DEFAULT NULL,
+  `CreationDate` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tblcourse`
+--
+
+INSERT INTO `tblcourse` (`ID`, `BranchName`, `CourseName`, `CreationDate`) VALUES
+(10, 'BCSE', 'BCSE', '2024-04-19 13:39:11');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblsuballocation`
+--
+
+CREATE TABLE `tblsuballocation` (
+  `ID` int(5) NOT NULL,
+  `CourseID` int(5) DEFAULT NULL,
+  `Teacherempid` varchar(100) DEFAULT NULL,
+  `Subid` int(5) DEFAULT NULL,
+  `AllocationDate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tblsuballocation`
+--
+
+INSERT INTO `tblsuballocation` (`ID`, `CourseID`, `Teacherempid`, `Subid`, `AllocationDate`) VALUES
+(6, 10, '54545', 11, '2024-04-19 13:44:28');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblsubject`
+--
+
+CREATE TABLE `tblsubject` (
+  `ID` int(5) NOT NULL,
+  `CourseID` int(5) DEFAULT NULL,
+  `SubjectFullname` varchar(200) DEFAULT NULL,
+  `SubjectShortname` varchar(200) DEFAULT NULL,
+  `SubjectCode` varchar(200) DEFAULT NULL,
+  `CreationDate` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tblsubject`
+--
+
+INSERT INTO `tblsubject` (`ID`, `CourseID`, `SubjectFullname`, `SubjectShortname`, `SubjectCode`, `CreationDate`) VALUES
+(11, 10, 'DBMS', 'DBMS', 'CSC 433', '2024-04-19 13:39:42');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblteacher`
+--
+
+CREATE TABLE `tblteacher` (
+  `ID` int(10) NOT NULL,
+  `EmpID` varchar(50) DEFAULT NULL,
+  `FirstName` varchar(200) DEFAULT NULL,
+  `LastName` varchar(200) DEFAULT NULL,
+  `MobileNumber` bigint(10) DEFAULT NULL,
+  `Email` varchar(200) DEFAULT NULL,
+  `Gender` varchar(200) DEFAULT NULL,
+  `Dob` varchar(200) DEFAULT NULL,
+  `CourseID` int(5) DEFAULT NULL,
+  `Religion` varchar(200) DEFAULT NULL,
+  `Address` mediumtext DEFAULT NULL,
+  `Password` varchar(200) DEFAULT NULL,
+  `ProfilePic` varchar(200) DEFAULT NULL,
+  `JoiningDate` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tblteacher`
+--
+
+INSERT INTO `tblteacher` (`ID`, `EmpID`, `FirstName`, `LastName`, `MobileNumber`, `Email`, `Gender`, `Dob`, `CourseID`, `Religion`, `Address`, `Password`, `ProfilePic`, `JoiningDate`) VALUES
+(5, '54545', 'Jebunnesa ', 'Sara', 199999999, 'Sara@iubat.edu', 'Female', '1990-04-30', 10, 'Islam', 'Uttara', NULL, 'f3ccdd27d2000e3f9255a7e3e2c488001713534230.jpg', '2024-04-19 13:54:23');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbladmin`
+--
+ALTER TABLE `tbladmin`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `tblcourse`
+--
+ALTER TABLE `tblcourse`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `tblsuballocation`
+--
+ALTER TABLE `tblsuballocation`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `tblsubject`
+--
+ALTER TABLE `tblsubject`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `tblteacher`
+--
+ALTER TABLE `tblteacher`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbladmin`
+--
+ALTER TABLE `tbladmin`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tblcourse`
+--
+ALTER TABLE `tblcourse`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `tblsuballocation`
+--
+ALTER TABLE `tblsuballocation`
+  MODIFY `ID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `tblsubject`
+--
+ALTER TABLE `tblsubject`
+  MODIFY `ID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `tblteacher`
+--
+ALTER TABLE `tblteacher`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ admin/includes/footer.php
@@ -0,0 +1,7 @@
+ <div class="row">
+                        <div class="col-lg-12">
+                            <div class="footer">
+                                <p>copyright © Epti Ara Era(22203177)</a></p>
+                            </div>
+                        </div>
+                    </div>
